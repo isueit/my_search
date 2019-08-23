1df1306f9b2cf0e9fb9d071ae5c13d8e7f0c2a5d
Tyler Gerwig <eit_tcgerwig@iastate.edu>
2019-08-23 15:55:41 -0500

    Removed cse_search dependency, fixed back button results in wrong radio selected

--- my_search.js
+++ my_search.js
@@ -1,8 +1,10 @@
-function add_param() {
-  var searchUrl = document.location;
-  var node = document.createElement('INPUT');
-  node.type = 'hidden';
-  node.name = 'switched';
-  node.value = true;
-  document.getElementById('my-search-toggle-form').appendChild(node);
+//Basic JS to use google custom search
+function my_search_js_request() {
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByClassName('cse_script')[0];
+  s.parentNode.insertBefore(gcse, s);
 }
+//This function is also called when there is an ajax on_change event for the search wideness radios
